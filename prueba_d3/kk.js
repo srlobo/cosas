06b5894ed8f952817739bd2bf5a4ac3359a21e4c
Felix Ortega <fortegah@bankia.com>
2015-01-12 16:49:11 +0100

    Version funcional

--- prueba_d3/kk.js
+++ prueba_d3/kk.js
@@ -2,23 +2,32 @@ function init(){
 	console.log("init");
 	console.log(data_json.conditions);
 
-	var margin = {top: 5, right: 100, bottom: 5, left: 100},
-	width = 600 - margin.right - margin.left,
-	height = 200 - margin.top - margin.bottom;
+	width = 600;
+	height = 250;
 
 	var i = 0;
 
 	var tree = d3.layout.tree()
-		.size([height, width]); 
+		.size([height, width]);
 
 	var diagonal = d3.svg.diagonal()
+		.source(function(d) {
+			var x = d.source.x;
+			var y = d.source.y;
+			return {y: y - 30, x: x};
+		})
+		.target(function(d) {
+			var x = d.target.x;
+			var y = d.target.y;
+			return {y: y + 60, x: x};
+		})
 		.projection(function(d) { return [d.y, d.x]; });
 
 	var svg = d3.select("body").append("svg")
-		.attr("width", width + margin.right + margin.left)
-		.attr("height", height + margin.top + margin.bottom)
+		.attr("width", width)
+		.attr("height", height)
 		.append("g")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		.attr("transform", "translate(" + (width - 60) + ", 0)");
 
 	var root = cond2tree(data_json.conditions);
 	console.log(root);
@@ -29,7 +38,7 @@ function init(){
 		var nodes = tree.nodes(source),
 			links = tree.links(nodes);
 
-		nodes.forEach(function(d) { d.y = d.depth * 100; });
+		nodes.forEach(function(d) { d.y = d.depth * -120; });
 
 		var node = svg.selectAll("g.node")
 			.data(nodes, function(d) { return d.id || (d.id = ++i); });
@@ -38,9 +47,8 @@ function init(){
 			.attr("class", "node")
 			.attr("transform", function(d) { 
 			return "translate(" + d.y + "," + d.x + ")"; });
-
 		nodeEnter.append("rect")
-			.attr("width", "60")
+			.attr("width", "90")
 			.attr("height", "30")
 			.attr("rx", "5")
 			.attr("ry", "5")
@@ -48,13 +56,147 @@ function init(){
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
 			.attr("transform", "translate(-30, -15)");
+		nodeEnter.append("rect")
+			.attr("width", "20")
+			.attr("height", "24")
+			.attr("rx", "2")
+			.attr("ry", "2")
+			.style("fill", "#fff")
+			.style("stroke", "#000")
+			.style("stroke-width", "1")
+			.attr("transform", "translate(35, -12)");
 
-		nodeEnter.append("text")
-			//.attr("x", "30")
+
+		var listNodes = nodeEnter.filter(function(d){
+			return d.cond_type=="LIST";});
+		for(var i = 8;i >= -8;i -= 4){
+			listNodes.append("line")
+				.attr("x1", "37")
+				.attr("y1", i)
+				.attr("x2", "53")
+				.attr("y2", i)
+				.style("stroke", "#000")
+				.style("stroke-width", "1")
+		}
+		listNodes.append("text")
 			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d) {
+				return d.cond_var;
+			})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, -7)");
+		listNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d) {
+				return "en lista " + d.list_id;
+			})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, 8)");
+
+
+		var dateNodes = nodeEnter.filter(function(d){
+			return d.cond_type=="DATE";});
+		for(var i = 39;i <= 51;i += 4){
+			dateNodes.append("line")
+				.attr("x1", i)
+				.attr("y1", "-14")
+				.attr("x2", i)
+				.attr("y2", "-10")
+				.style("stroke", "#000")
+				.style("stroke-width", "1")
+		}
+		dateNodes.append("rect")
+			.attr("width", "16")
+			.attr("height", "14")
+			.attr("rx", "2")
+			.attr("ry", "2")
+			.style("fill", "#fff")
+			.style("stroke", "#000")
+			.style("stroke-width", "1")
+			.attr("transform", "translate(37, -6)");
+		dateNodes.append("text")
 			.attr("text-anchor", "middle")
-			.text(function(d) { return d.name; })
+			.attr("x", "45")
+			.attr("y", "5")
+			.style("font-weight", "bold")
+			.text(function(d){return d.cd_date.split("T")[0].split("-")[2];})
 			.style("fill-opacity", 1);
+		dateNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d) {
+				switch(d.cd_comparision){
+					case "GT":
+						return "Después de";
+						break;;
+					case "LT":
+						return "Antes de";
+						break;;
+				}
+			})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, -7)");
+		dateNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d) { return d.cd_date.split("T")[0];})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, 8)");
+
+
+		var andNodes = nodeEnter.filter(function(d){
+			return d.cond_type=="AND";});
+		andNodes.append("path")
+			.attr("d", "M3,2 L3,22 A10 10 0 0 0 3 2 Z")
+			.style("stroke", "#000")
+			.style("stroke-width", "1")
+			.style("fill", "#fff")
+			.attr("transform", "translate(38, -12)");
+		andNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text("AND")
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, -7)");
+		andNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d){return d.cond_name;})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, 8)");
+
+
+		var orNodes = nodeEnter.filter(function(d){
+			return d.cond_type=="OR";});
+		orNodes.append("path")
+			.attr("d", "M3 20 A14 14 0 0 0 3 4 M3 20 A20 20 0 0 0 17 12 A20 20 0 0 0 3 4")
+			.style("stroke", "#000")
+			.style("stroke-width", "1")
+			.style("fill", "#fff")
+			.attr("transform", "translate(35, -12)");
+		orNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text("OR")
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, -7)");
+		orNodes.append("text")
+			.attr("dy", ".35em")
+			.attr("text-anchor", "left")
+			.text(function(d){return d.cond_name;})
+			.style("fill-opacity", 1)
+			.style("font-size", 10)
+			.attr("transform", "translate(-27, 8)");
+
 
 		// Declare the links
 		var link = svg.selectAll("path.link")
