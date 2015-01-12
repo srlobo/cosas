bee5fa4aa77d875894be1f9d9c7358e9c8e5c7ef
Félix Ortega <fortegah@gmail.com>
2015-01-12 21:30:55 +0100

    Orientación vertical

--- prueba_d3/kk.js
+++ prueba_d3/kk.js
@@ -2,32 +2,30 @@ function init(){
 	console.log("init");
 	console.log(data_json.conditions);
 
-	width = 600;
-	height = 250;
-
-	var i = 0;
+	var width = 1000;
+	var height = 1000;
 
 	var tree = d3.layout.tree()
-		.size([height, width]);
+		.nodeSize([100, 30]);
 
 	var diagonal = d3.svg.diagonal()
 		.source(function(d) {
 			var x = d.source.x;
 			var y = d.source.y;
-			return {y: y - 30, x: x};
+			return {y: y + 10, x: x};
 		})
 		.target(function(d) {
 			var x = d.target.x;
 			var y = d.target.y;
-			return {y: y + 60, x: x};
+			return {y: y - 10, x: x};
 		})
-		.projection(function(d) { return [d.y, d.x]; });
+		.projection(function(d) { return [d.x, d.y]; });
 
 	var svg = d3.select("body").append("svg")
-		.attr("width", width)
-		.attr("height", height)
+		.attr("width", "1000")
+		.attr("height", "1000")
 		.append("g")
-		.attr("transform", "translate(" + (width - 60) + ", 0)");
+		.attr("transform", "translate(300, 30)");
 
 	var root = cond2tree(data_json.conditions);
 	console.log(root);
@@ -38,15 +36,15 @@ function init(){
 		var nodes = tree.nodes(source),
 			links = tree.links(nodes);
 
-		nodes.forEach(function(d) { d.y = d.depth * -120; });
+		nodes.forEach(function(d) { d.y = d.depth * 50; });
 
 		var node = svg.selectAll("g.node")
-			.data(nodes, function(d) { return d.id || (d.id = ++i); });
+			.data(nodes, function(d) {return d.id});
 
 		var nodeEnter = node.enter().append("g")
 			.attr("class", "node")
 			.attr("transform", function(d) { 
-			return "translate(" + d.y + "," + d.x + ")"; });
+			return "translate(" + d.x + "," + d.y + ")"; });
 		nodeEnter.append("rect")
 			.attr("width", "90")
 			.attr("height", "30")
@@ -55,7 +53,7 @@ function init(){
 			.style("fill", "#fff")
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
-			.attr("transform", "translate(-30, -15)");
+			.attr("transform", "translate(-45, -15)");
 		nodeEnter.append("rect")
 			.attr("width", "20")
 			.attr("height", "24")
@@ -64,16 +62,16 @@ function init(){
 			.style("fill", "#fff")
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
-			.attr("transform", "translate(35, -12)");
+			.attr("transform", "translate(20, -12)");
 
 
 		var listNodes = nodeEnter.filter(function(d){
 			return d.cond_type=="LIST";});
 		for(var i = 8;i >= -8;i -= 4){
 			listNodes.append("line")
-				.attr("x1", "37")
+				.attr("x1", "22")
 				.attr("y1", i)
-				.attr("x2", "53")
+				.attr("x2", "38")
 				.attr("y2", i)
 				.style("stroke", "#000")
 				.style("stroke-width", "1")
@@ -86,7 +84,7 @@ function init(){
 			})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, -7)");
+			.attr("transform", "translate(-42, -7)");
 		listNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
@@ -95,12 +93,12 @@ function init(){
 			})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, 8)");
+			.attr("transform", "translate(-42, 8)");
 
 
 		var dateNodes = nodeEnter.filter(function(d){
 			return d.cond_type=="DATE";});
-		for(var i = 39;i <= 51;i += 4){
+		for(var i = 24;i <= 36;i += 4){
 			dateNodes.append("line")
 				.attr("x1", i)
 				.attr("y1", "-14")
@@ -117,10 +115,10 @@ function init(){
 			.style("fill", "#fff")
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
-			.attr("transform", "translate(37, -6)");
+			.attr("transform", "translate(22, -6)");
 		dateNodes.append("text")
 			.attr("text-anchor", "middle")
-			.attr("x", "45")
+			.attr("x", "30")
 			.attr("y", "5")
 			.style("font-weight", "bold")
 			.text(function(d){return d.cd_date.split("T")[0].split("-")[2];})
@@ -140,14 +138,14 @@ function init(){
 			})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, -7)");
+			.attr("transform", "translate(-42, -7)");
 		dateNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
 			.text(function(d) { return d.cd_date.split("T")[0];})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, 8)");
+			.attr("transform", "translate(-42, 8)");
 
 
 		var andNodes = nodeEnter.filter(function(d){
@@ -157,21 +155,21 @@ function init(){
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
 			.style("fill", "#fff")
-			.attr("transform", "translate(38, -12)");
+			.attr("transform", "translate(23, -12)");
 		andNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
 			.text("AND")
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, -7)");
+			.attr("transform", "translate(-42, -7)");
 		andNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
 			.text(function(d){return d.cond_name;})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, 8)");
+			.attr("transform", "translate(-42, 8)");
 
 
 		var orNodes = nodeEnter.filter(function(d){
@@ -181,21 +179,21 @@ function init(){
 			.style("stroke", "#000")
 			.style("stroke-width", "1")
 			.style("fill", "#fff")
-			.attr("transform", "translate(35, -12)");
+			.attr("transform", "translate(20, -12)");
 		orNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
 			.text("OR")
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, -7)");
+			.attr("transform", "translate(-42, -7)");
 		orNodes.append("text")
 			.attr("dy", ".35em")
 			.attr("text-anchor", "left")
 			.text(function(d){return d.cond_name;})
 			.style("fill-opacity", 1)
 			.style("font-size", 10)
-			.attr("transform", "translate(-27, 8)");
+			.attr("transform", "translate(-42, 8)");
 
 
 		// Declare the links
