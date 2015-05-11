eadcafdd4ce397d1a84c7bc0b18cd8cdc018796e
Félix Ortega <fortegah@gmail.com>
2015-05-11 21:56:49 +0200

    Pruebas con la libreria masonry

---
+++ montaje/prog.js
@@ -0,0 +1,58 @@
+var types = [{
+	height: 10,
+	width: 15,
+	number: 6
+},
+{
+	height: 15,
+	width: 10,
+	number: 6
+},
+{
+	height: 20,
+	width: 30,
+	number: 0
+},
+{
+	height: 30,
+	width: 20,
+	number: 0
+},
+{
+	height: 13,
+	width: 18,
+	number: 6
+},
+{
+	height: 18,
+	width: 13,
+	number: 6
+}
+];
+
+function generate_sequence(number){
+	var i = 0;
+	_.each(types, function(value){
+		for(j = 0;j < value.number;++j){
+			var el = $("<div id='cuadro_" + i + "' class='photo'>");
+			el.height(value.height * 10);
+			el.width(value.width * 10);
+			$("#container").append(el)
+			++i;
+		}
+	});
+	var divs = $("#container").children();
+	while (divs.length) {
+		$("#container").append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+	}
+}
+
+$(document).ready(function(){
+	generate_sequence(20);
+	new Masonry(("#container"), {
+		"columnWidth": 10,
+		"gutter": 10,
+	})
+});
+
+
